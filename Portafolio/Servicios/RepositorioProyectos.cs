c8baef6e8ef0d753e7147efa9384db08771f8d5b
Alejandro Matos <isc.a.matos21@gmail.com>
2023-02-27 13:27:23 -0600

    Agregar archivos de proyecto.

---
+++ Portafolio/Servicios/RepositorioProyectos.cs
@@ -0,0 +1,48 @@
+﻿using Portafolio.Models;
+
+namespace Portafolio.Servicios
+{
+    public interface IRepositorioProyectos
+    {
+        List<Proyecto> ObtenerProyectos();
+
+    }
+    public class RepositorioProyectos: IRepositorioProyectos
+    {
+
+        public List<Proyecto> ObtenerProyectos()
+        {
+            return new List<Proyecto>()
+            {
+                new Proyecto
+            {
+                Titulo = "Amazon",
+                Descripcion = "E-Commerce realizado en ASP.NET Core",
+                Link = "https://www.amazon.com.mx",
+                ImagenURL = "/imagenes/amazon.png"
+            },
+                  new Proyecto
+            {
+                Titulo = "New York Times",
+                Descripcion = "E-Commerce realizado en ASP.NET Core",
+                Link = "https://nytimes.com",
+                ImagenURL = "/imagenes/nyt.png"
+            },
+                    new Proyecto
+            {
+                Titulo = "Reddit",
+                Descripcion = "E-Commerce realizado en ASP.NET Core",
+                Link = "https://reddit.com",
+                ImagenURL = "/imagenes/reddit.png"
+            },
+                      new Proyecto
+            {
+                Titulo = "Steam",
+                Descripcion = "Tienda en línea para comprar videojuegos",
+                Link = "https://store.steampowered.com",
+                ImagenURL = "/imagenes/steam.png"
+            },
+            };
+        }
+    }
+}
